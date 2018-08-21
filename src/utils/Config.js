406488b64476260303775161df3056b0ec15c2dd
abanglaile <abanglaile@users.noreply.github.com>
2018-08-21 18:25:34 +0800

    修改server_url

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -2,8 +2,8 @@
 const config = {
   // server_url: "http://39.108.85.119:3000",
   // server_url : "https://www.kmap.xin/klmanager",
-  // server_url: "http://www.zhiqiu.pro/api",
-  server_url: "http://172.20.10.2:7001/api",
+  server_url: "http://www.zhiqiu.pro/api",
+  // server_url: "http://172.20.10.3:7001/api",
   // appid : "wx6f3a777231ad1747",
   appid : "wx1dc40895f45755ba",
   redirect_uri : "http%3a%2f%2fwww.zhiqiu.pro%2fmobile-zq%2finvite",
